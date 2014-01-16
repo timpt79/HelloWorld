a20de6baa9b4bd51a0645df2ef6263589fc85a37
timpt79 <timpt79@gmail.com>
2014-01-16 14:12:18 -0700

    initial setup

---
+++ src/hello/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author Tim
+ */
+public class Hello {
+
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }//end sayHello method
+}//end Hello class
